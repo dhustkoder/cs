3f20ece75c2b48727548fcb5ddc7c8c01ec7ff8f
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-01 13:18:02 +0000

    edited build script to build C and C++
    rev

--- build.sh
+++ build.sh
@@ -1,40 +1,74 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)
-SRC_DIR="${BASEDIR}/src"
-OUTPUT_DIR="${BASEDIR}/build"
-
-if ["$CC" == ""]; then
-	CC=gcc
-fi
-
-if [ ! -d $OUTPUT_DIR ]; then
-	mkdir $OUTPUT_DIR
-fi
+SRCDIR="${BASEDIR}/src"
+BUILDDIR="${BASEDIR}/build"
 
 
-compile ()
+
+compileCC ()
 {
-	mode=$1
-	cargs=$2
+	# compile C and C++ code
+	C_OUTPUT_DIR="${BUILDDIR}/c"
+	CXX_OUTPUT_DIR="${BUILDDIR}/cpp"
+	C_SRC_DIR="${SRCDIR}/c"
+	CXX_SRC_DIR="${SRCDIR}/cpp"
+	CFLAGS="-std=c11 -Wall -Wextra -pedantic"
+	CFLAGS_DEBUG="-O0 -ggdb -fsanitize=address"
+	CFLAGS_RELEASE="-O3"
+	CXXFLAGS="-std=c++11 -Wall -Wextra -pedantic"
+	CXXFLAGS_DEBUG=$CFLAGS_DEBUG
+	CXXFLAGS_RELEASE=$CFLAGS_RELEASE
+
+	if ["$CC" == ""]; then
+		CC=gcc
+	fi
+
+	if ["$CXX" == ""]; then
+		CXX=g++
+	fi
+
+	mode="${1}"
+
+	if [[ $mode == "" || $mode == "debug" ]]; then
+		mode="debug"
+		CFLAGS="${CFLAGS} ${CFLAGS_DEBUG}"
+		CXXFLAGS="${CXXFLAGS} ${CXXFLAGS_DEBUG}"
+	elif [[ $mode == "release" ]]; then
+		CFLAGS="${CFLAGS} ${CFLAGS_RELEASE}"
+		CXXFLAGS="${CXXFLAGS} ${CXXFLAGS_RELEASE}"
+	else
+		echo "compileCC: Unknown mode \'${mode}\'"
+		return
+	fi
 
 	echo "Building in ${mode} mode"
 
-	for file in $(find $SRC_DIR -name '*.c'); do
-		fname=$(basename $file)
-		outname="${OUTPUT_DIR}/${fname%".c"}-${mode}.out"
-		echo "${file}: ${outname}"
-		$CC -std=c11 -Wall -Wextra -pedantic $cargs -I $SRC_DIR -o $outname $file
+	FILETYPES=("*.c" "*.cpp")
+	OUTDIRS=("$C_OUTPUT_DIR" "$CXX_OUTPUT_DIR")
+	SRCDIRS=("$C_SRC_DIR" "$CXX_SRC_DIR")
+	COMPILERS=("$CC" "$CXX")
+	FLAGS=("$CFLAGS" "$CXXFLAGS")
+
+	for i in 0 1; do
+
+		if [ ! -d ${SRCDIRS[$i]} ]; then
+			continue
+		fi
+
+		for file in $(find ${SRCDIRS[$i]} -name ${FILETYPES[$i]}); do
+			fname=$(basename $file)
+
+			if [ ! -d ${OUTDIRS[$i]} ]; then
+				mkdir -p ${OUTDIRS[$i]}
+			fi
+
+			outname="${OUTDIRS[$i]}/${fname%${FILETYPES[$i]}}-${mode}.out"
+			echo "${file}: ${outname}"
+			${COMPILERS[$i]} ${FLAGS[$i]} -I ${SRCDIRS[$i]} -o $outname $file
+		done
 	done
 }
 
-
-if [[ "${1}" == "debug" || "${1}" == "" ]]; then
-	compile "debug" "-O0 -ggdb -fsanitize=address"
-elif [[ "${1}" == "release" ]]; then
-	compile "release" "-O3"
-else
-	echo "Usage: ${0} [mode]"
-	echo "mode: debug, release"
-fi
+compileCC $@
 
