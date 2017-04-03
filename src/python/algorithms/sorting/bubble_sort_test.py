acbafa42e06c7ceb0a647768f883db2316a754a8
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-03 17:15:48 +0000

    python printdata

--- src/python/algorithms/sorting/bubble_sort_test.py
+++ src/python/algorithms/sorting/bubble_sort_test.py
@@ -9,6 +9,11 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(sys.argv[0])) + "/../../")
 from bubble_sort import bubble_sort
 from common import *
 
+
+
+
+
+
 nums = make_int_list_from_strings(sys.argv[1:])
 
 if len(nums) < 2:
@@ -16,17 +21,12 @@ if len(nums) < 2:
     sys.exit(os.EX_USAGE)
 
 print ("UNSORTED:")
-
-for n in nums:
-    print(n)
+print_data(nums)
 
 bubble_sort(nums, lambda a, b: (a < b))
 
 print("SORTED:")
-
-for n in nums:
-    print(str(n))
-
+print_data(nums)
 
 sys.exit(os.EX_OK)
 
--- src/python/common.py
+++ src/python/common.py
@@ -4,6 +4,11 @@ def make_int_list_from_strings(strs):
     l = []
     for elem in strs:
         l.append(int(elem))
+
     return l
 
+
+def print_data(data):
+    for elem in data:
+        print(elem)
 
