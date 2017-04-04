08d204b8e229bf8d795ca79c822a815f5a73ff18
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-04 21:41:41 +0000

    python Insertion Sort

---
+++ src/python/algorithms/sorting/insertion_sort.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+def insertion_sort(data, compare):
+
+    for n in range(1, len(data)):
+        j = n
+        while j > 0 and compare(data[j], data[j - 1]):
+            aux = data[j - 1]
+            data[j - 1] = data[j]
+            data[j] = aux
+            j = j - 1
+
+    return data
+
---
+++ src/python/algorithms/sorting/insertion_sort_test.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+import sys
+import os
+
+# python src top dir (cs/src/python)
+sys.path.insert(0, os.path.dirname(os.path.realpath(sys.argv[0])) + "/../../")
+
+from insertion_sort import *
+from common import *
+
+
+
+
+
+
+nums = make_int_list_from_strings(sys.argv[1:])
+
+if len(nums) < 2:
+    print("Usage: " + sys.argv[0] + " [list]")
+    sys.exit(os.EX_USAGE)
+
+print ("UNSORTED:")
+print_data(nums)
+print("SORTED:")
+print_data(insertion_sort(nums, lambda a, b: (a < b)))
+sys.exit(os.EX_OK)
+
