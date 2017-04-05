9c4704d126f6a2e97d764e431da38bf1b791fb30
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-05 02:02:40 +0000

    py quickSort

---
+++ src/python/algorithms/sorting/quick_sort.py
@@ -0,0 +1,45 @@
+#!/bin/python
+
+
+def quick_sort_part(l, r, data, compare):
+    p = l
+    l = l + 1
+    r = r - 1
+
+    while True:
+
+        while l < r and compare(data[l], data[p]):
+            l = l + 1
+
+        while r >= l and compare(data[p], data[r]):
+            r = r - 1
+        
+        if l >= r:
+            break
+
+        tmp = data[l]
+        data[l] = data[r]
+        data[r] = tmp
+
+        l = l + 1
+        r = r - 1
+
+    tmp = data[r]
+    data[r] = data[p]
+    data[p] = tmp
+
+    return r
+
+
+
+def qsort_impl(l, r, data, compare):
+    if (r - l) > 1:
+        p = quick_sort_part(l, r, data, compare)
+        qsort_impl(p + 1, r, data, compare)
+        qsort_impl(l, p, data, compare)
+
+
+def quick_sort(data, compare):
+    qsort_impl(0, len(data), data, compare)
+    return data
+
