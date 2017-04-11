482ac8f86d69afc1e7de84642d0c7fbe44ec0847
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-11 13:44:08 +0000

    cs/java/InsertionSort

---
+++ src/java/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package src.java.algorithms.sorting;
+import src.java.algorithms.sorting.SortingAlgorithm;
+import src.java.common.Tests;
+import java.util.*;
+
+
+public class InsertionSort implements SortingAlgorithm {
+
+	public <T extends Comparable<T>> void execute(List<T> data)
+	{
+		for (int i = 1; i < data.size(); ++i) {
+			for (int j = i; j > 0 && data.get(j).compareTo(data.get(j - 1)) < 0; --j) {
+				final T tmp = data.get(j);
+				data.set(j, data.get(j - 1));
+				data.set(j - 1, tmp);
+			}
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Tests.sort(args, new InsertionSort());
+	}
+}
