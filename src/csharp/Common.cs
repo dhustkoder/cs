d879445cb39f642656191a0c541ab7c8bd4cf7fb
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-14 19:01:47 +0000

    csharp Mono tests

--- src/csharp/Common.cs
+++ src/csharp/Common.cs
@@ -1,18 +1,27 @@
-
-
+//using Algorithms.Sorting.ISortingAlgoritm;
+using System;
+using System.Collections.Generic;
 
 namespace Common {
 
-
 	public class Tests {
 
+		public static void sort(string[] args)
+		{
+			var data = new List<int>();
+			foreach (var str in args)
+				data.Add(int.Parse(str));
 
+			foreach (var i in data)
+				System.Console.WriteLine(i);
+		}
 
 
 
 		public static void Main(string[] args)
 		{
-			System.Console.WriteLine("Hello World " + args[0]);
+			//System.Console.WriteLine("Hello World " + args[0]);
+			sort(args);
 		}
 
 
