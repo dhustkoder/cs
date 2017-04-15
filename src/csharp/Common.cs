456500f977d7f1670473a219782a7d30ceff2aa1
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-15 10:47:44 +0000

    adding C# and .gitignore update -

---
+++ src/csharp/Common.cs
@@ -0,0 +1,32 @@
+
+
+
+namespace Common {
+
+
+	public class Tests {
+
+
+
+
+
+		public static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello World " + args[0]);
+		}
+
+
+
+
+
+
+	}
+
+
+
+}
+
+
+
+
+
