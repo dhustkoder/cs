50b57b49bd895d32444dd706dab685723481fdfa
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-04-16 16:44:38 +0000

    cs/csharp/QuickSort

---
+++ src/csharp/algorithms/sorting/QuickSort.cs
@@ -0,0 +1,63 @@
+using Common;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+	public class QuickSort : ISortingAlgorithm
+	{
+
+		public void Execute<T>(List<T> data)
+		{
+			if (data.Count > 1)
+				QSort(0, data.Count, data);
+		}
+
+
+		private void QSort<T>(int beg, int end, List<T> data)
+		{
+			if ((end - beg) > 1) {
+				int r = Partitionate(beg, end, data);
+				QSort(r + 1, end, data);
+				QSort(beg, r, data);
+			}
+		}
+
+
+		private int Partitionate<T>(int beg, int end, List<T> data)
+		{
+			var comparer = Comparer<T>.Default;
+			int l = beg + 1;
+			int r = end - 1;
+
+			for (;;) {
+				while (l < r && comparer.Compare(data[l], data[beg]) < 0)
+					l += 1;
+				while (r >= l && comparer.Compare(data[r], data[beg]) > 0)
+					r -= 1;
+
+				if (l >= r)
+					break;
+
+				T tmp = data[l];
+				data[l] = data[r];
+				data[r] = tmp;
+
+				l += 1;
+				r -= 1;
+			}
+
+			T aux = data[r];
+			data[r] = data[beg];
+			data[beg] = aux;
+			return r;
+		}
+
+
+		public static int Main(string[] args)
+		{
+			return Tests.Sort(args, new QuickSort());
+		}
+
+	}
+}
